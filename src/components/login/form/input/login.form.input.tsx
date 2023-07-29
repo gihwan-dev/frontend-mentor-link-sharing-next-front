4e53d7ef4874100f5e92e5c0c84ea63b346c3f8c
최기환 <rlghks9806@gmail.com>
2023-07-29 18:02:51 +0900

    이메일 패스워드 검증 작업 필요

--- src/components/login/form/input/login.form.input.tsx
+++ src/components/login/form/input/login.form.input.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./login.form.input.module.scss";
+import { FocusEvent, useState } from "react";
 
 const LoginFormInput: React.FC<{
   icon: string;
@@ -7,12 +10,46 @@ const LoginFormInput: React.FC<{
   type: "email" | "password";
   label: string;
 }> = ({ icon, placeholder, type, label }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const onFocusHandler = (event: FocusEvent<HTMLInputElement>) => {
+    setIsActive(true);
+  };
+
+  const onBlurHandler = (event: FocusEvent<HTMLInputElement>) => {
+    setIsActive(false);
+  };
+
+  const emailValidationHandler = (targetEmail: string) => {};
+
+  const passwordValidationHandler = (targetPassword: string) => {};
+
+  const validateInput = (targetString: string) => {
+    switch (type) {
+      case "email":
+        emailValidationHandler(targetString);
+        break;
+      case "password":
+        passwordValidationHandler(targetString);
+        break;
+    }
+  };
+
   return (
     <div className={styles.container}>
       <label>{label}</label>
-      <div className={styles["input-container"]}>
+      <div
+        className={`${styles["input-container"]} ${
+          isActive ? styles.isActive : null
+        }`}
+      >
         <Image src={icon} alt={"icon"} />
-        <input type={type} placeholder={placeholder} />
+        <input
+          onBlur={onBlurHandler}
+          onFocus={onFocusHandler}
+          type={type}
+          placeholder={placeholder}
+        />
       </div>
     </div>
   );
