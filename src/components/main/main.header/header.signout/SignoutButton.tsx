e5ccfaa4e7403e8b1d9738edeb1e6410e2a4c752
최기환 <rlghks3004@gmail.com>
2023-12-11 20:02:12 +0900

    .

--- src/components/main/main.header/header.signout/SignoutButton.tsx
+++ src/components/main/main.header/header.signout/SignoutButton.tsx
@@ -1,9 +1,22 @@
+"use client";
+
 import styles from "./header.signout.module.scss";
-import {signOut} from "next-auth/react";
 import SignoutIcon from "public/assets/images/Vector.svg"
+import {SERVER_URL} from "@/const";
 
 export default function SignoutButton() {
-    return <button onClick={() =>signOut()} className={styles.btn}>
+
+    async function signOutHandler() {
+        try {
+            const response = await fetch(`${SERVER_URL}/auth/signOut`)
+        } catch (e) {
+
+        } finally {
+
+        }
+    }
+
+    return <button onClick={signOutHandler} className={styles.btn}>
         <span className={styles.icon}><SignoutIcon /></span>
         <span className={styles.text}>Sign out</span>
     </button>
