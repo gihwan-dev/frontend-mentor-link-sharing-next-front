67424a75bcb14fe57689cef50de50786832ade45
최기환 <rlghks3004@gmail.com>
2023-12-13 14:18:55 +0900

    .

--- src/components/main/main.header/header.signout/SignoutButton.tsx
+++ src/components/main/main.header/header.signout/SignoutButton.tsx
@@ -3,14 +3,27 @@
 import styles from "./header.signout.module.scss";
 import SignoutIcon from "public/assets/images/Vector.svg"
 import {SERVER_URL} from "@/const";
+import {useRouter} from "next/navigation";
 
 export default function SignoutButton() {
+    const router = useRouter();
 
     async function signOutHandler() {
         try {
-            const response = await fetch(`${SERVER_URL}/auth/signOut`)
+            const response = await fetch(`${SERVER_URL}/auth/signout`, {
+                credentials: "include",
+            });
+            if (!response.ok) {
+                window.alert("네트워크 상태가 불안정합니다. 다시 시도해 주세요.");``
+                return;
+            }
+            const data = await response.json();
+            window.alert(data.message);
+            router.replace("/login")
+            return;
         } catch (e) {
-
+            window.alert("네트워크 상태가 불안정합니다. 다시 시도해 주세요.");
+            return;
         } finally {
 
         }
