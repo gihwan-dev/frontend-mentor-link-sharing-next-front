fcecf725ba88d4722a2c6cf86141095507319fd4
최기환 <rlghks3004@gmail.com>
2024-01-04 16:57:44 +0900

    서버 URL 수정

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,6 +1,4 @@
-export const SERVER_URL =
-  // "https://fonrtend-mentor-link-sharing-gihwan-dev.azurewebsites.net";
-  "http://localhost:8000";
+export const SERVER_URL = "https://link-sharing.azurewebsites.net";
 
 export const SCROLL = {
   DRAG_SCROLL_THRESHOLD: 50,
