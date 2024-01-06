8020e2f92864dd1a3b45c904d6384791c44d0898
최기환 <rlghks3004@gmail.com>
2024-01-06 23:12:01 +0900

    .

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,4 +1,4 @@
-export const SERVER_URL = "https://link-sharing.azurewebsites.net";
+export const SERVER_URL = "https://link-sharing-server.gihwan-dev.net";
 
 export const SCROLL = {
   DRAG_SCROLL_THRESHOLD: 50,
